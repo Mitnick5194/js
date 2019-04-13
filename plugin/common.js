ea050ffc4e134b200ad5cadf0f9fe70185fec88e
Mitnick5194 <qq519441630@gmail.com>
2019-04-14 01:36:21 +0800

    优化插件

--- plugin/common.js
+++ plugin/common.js
@@ -391,6 +391,9 @@
 	 	//后退
 	 	window.history.pushState(null, null, "#"+anchor);
 	 	mask.removeClass("slide-win-mask-hide").addClass("slide-win-mask-show").show();
+	 	//禁止页面滚动时下面的也跟着滚动
+ 		BODY.addClass("disable-scroll");
+ 		$("html").addClass("disable-scroll")
 	 	trans("show");
 	 	var oldTitle = DOC[0].title;
 	 	opts.oldTitle = oldTitle;
@@ -411,6 +414,9 @@
 
 	 function hide(callback){
 	 	mask.removeClass("slide-win-mask-show").addClass("slide-win-mask-hide").show();
+		//解除禁止滑动
+ 		BODY.removeClass("disable-scroll");
+ 		$("html").removeClass("disable-scroll")
 	 	trans("hide");
 	 	DOC[0].title = opts.oldTitle;
 	 	var timing = setTimeout(function(){
@@ -426,7 +432,11 @@
 	 this.hide = function(callback){
 	 	hide(callback);
 	 }
- }
+  }
+
+  function FunnelLoading(){
+  	
+  }
 
  /**
   * base64编码
